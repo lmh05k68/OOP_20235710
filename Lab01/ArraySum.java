c06d33c47cd9025e7ef241047d85b2eca2225d95
lmh05k68 <158491940+lmh05k68@users.noreply.github.com>
2025-03-01 20:11:57 +0700

    Add files via upload

---
+++ Lab01/ArraySum.java
@@ -0,0 +1,30 @@
+package sum;
+import java.util.Scanner;
+public class ArraySum {
+	public static void main(String[] args) {
+		Scanner keyboardScanner = new Scanner(System.in);
+		System.out.print("So phan tu cua mang: ");
+		int n = ketboard.nextInt();
+		int[] arr = new int[n];
+		System.out.println("Nhap phan tu:");
+		for(int i = 0; i < n; i++) {
+			arr[i] = keyboard.nextInt();
+		}
+		for(int i = 0; i < n-1; i++) {
+			for(int j = 0; j < n-i-1; j++) {
+				if(arr[j] > arr[j+1]) {
+					int tmp = arr[j];
+					arr[j] = arr[j+1];
+					arr[j+1] = tmp;
+				}
+			}
+		}
+		int sum = 0;
+		for(int i = 0; i < n; i++) {
+			sum += arr[i];
+		}
+		double avg = (double)sum/n;
+		System.out.println("Tong: " + sum);
+		System.out.println("Trung binh: " + avg);
+	}
+}
